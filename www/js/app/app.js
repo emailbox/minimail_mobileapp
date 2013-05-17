4e72bdc76edb0630bc890272dbdf761d9f4ce361
Nicholas Reed <nicholas.a.reed@gmail.com>
2013-05-16 23:44:45 -0700

    version bumps

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -16,7 +16,7 @@ var App = {
 	Events: 	 _.extend({}, Backbone.Events),
 	Data: 		 {
 		// tmp_contacts: [], // testing contacts
-		version: "0.0.20",
+		version: "0.0.21",
 		online: true,
 		LoggedIn: false, // Logged into minimail servers
 		notifications_queue: [],
