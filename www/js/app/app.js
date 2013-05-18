308059f62606044710ddeaffa75ba94c1eb07b6b
Nicholas Reed <nicholas.a.reed@gmail.com>
2013-05-18 00:31:14 -0700

    version 0.0.25

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -16,7 +16,7 @@ var App = {
 	Events: 	 _.extend({}, Backbone.Events),
 	Data: 		 {
 		// tmp_contacts: [], // testing contacts
-		version: "0.0.21",
+		version: "0.0.25",
 		online: true,
 		LoggedIn: false, // Logged into minimail servers
 		notifications_queue: [],
